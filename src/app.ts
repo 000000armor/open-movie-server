e3f6d3110cab12c5fb701bc69599ef7722651228
David Sevikian <davidsevikian@Davids-MacBook-Pro.local>
2023-11-30 12:41:11 +0400

    edit app

--- src/app.ts
+++ src/app.ts
@@ -5,5 +5,5 @@ import { moviesRoutes } from './routes/movies-routes.js';
 export const app = express();
 
 app.use(extendContextWithServices as unknown as RequestHandler);
-app.use(express.json()).use('/api/example', () => {});
+app.use(express.json());
 app.use(moviesRoutes);
